e51caee58f8999de22541caf0c35561ae832f2d5
xiefei <410337505@qq.com>
2014-02-24 14:44:19 +0800

    Revert "iOS照片处理"
    This reverts commit b5490183bc8955b17c61c9d408f9665708daf321.

--- CoreImage/ViewController.h
+++ CoreImage/ViewController.h
@@ -8,19 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
-
-@property (strong, nonatomic) IBOutlet UIImageView *imgV;
-@property (strong, nonatomic) IBOutlet UISlider *slider;
-@property (strong, nonatomic) IBOutlet UISlider *slider2;
-@property (strong, nonatomic) IBOutlet UISlider *slider3;
-
-- (IBAction)changeValue:(id)sender;
-- (IBAction)changeValue2:(id)sender;
-- (IBAction)changeValue3:(id)sender;
-
-- (IBAction)loadPhoto:(id)sender;
-- (IBAction)savePhoto:(id)sender;
-- (IBAction)resetImage:(id)sender;
+@interface ViewController : UIViewController
 
 @end
